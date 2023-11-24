f6700701cfc681f9985a3097848ab89ac0abc232
ArthurDEV44 <arthurjean-dev@outlook.fr>
2023-11-24 16:58:49 +0100

    test husky

--- back/auth.js
+++ back/auth.js
@@ -37,7 +37,7 @@ router.post("/signup", async (ctx) => {
       errorMessage += "E-mail déjà utilisé.";
     }
 
-    ctx.status = 200;
+    ctx.status = 400;
     ctx.body = { message: errorMessage };
     return;
   }
